8532280536900fe8fd796954306f5bb1433e3886
FWiiii <843189132@qq.com>
2024-12-18 20:51:56 +0800

    chore:eslint add

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,14 +1,16 @@
-import { Context } from 'telegraf';
-import createDebug from 'debug';
+import type { Context } from 'telegraf'
+import createDebug from 'debug'
 
-import { author, name, version } from '../../package.json';
+import { author, name, version } from '../../package.json'
 
-const debug = createDebug('bot:about_command');
+const debug = createDebug('bot:about_command')
 
-const about = () => async (ctx: Context) => {
-  const message = `*${name} ${version}*\n${author}`;
-  debug(`Triggered "about" command with message \n${message}`);
-  await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' });
-};
+function about() {
+  return async (ctx: Context) => {
+    const message = `*${name} ${version}*\n${author}`
+    debug(`Triggered "about" command with message \n${message}`)
+    await ctx.replyWithMarkdownV2(message, { parse_mode: 'Markdown' })
+  }
+}
 
-export { about };
+export { about }
--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -1,22 +1,25 @@
-import { Context } from 'telegraf';
-import createDebug from 'debug';
+import type { Context } from 'telegraf'
+import createDebug from 'debug'
 
-const debug = createDebug('bot:greeting_text');
+const debug = createDebug('bot:greeting_text')
 
-const replyToMessage = (ctx: Context, messageId: number, string: string) =>
-  ctx.reply(string, {
+function replyToMessage(ctx: Context, messageId: number, string: string) {
+  return ctx.reply(string, {
     reply_parameters: { message_id: messageId },
-  });
+  })
+}
 
-const greeting = () => async (ctx: Context) => {
-  debug('Triggered "greeting" text command');
+function greeting() {
+  return async (ctx: Context) => {
+    debug('Triggered "greeting" text command')
 
-  const messageId = ctx.message?.message_id;
-  const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`;
+    const messageId = ctx.message?.message_id
+    const userName = `${ctx.message?.from.first_name} ${ctx.message?.from.last_name}`
 
-  if (messageId) {
-    await replyToMessage(ctx, messageId, `Hello, ${userName}!`);
+    if (messageId) {
+      await replyToMessage(ctx, messageId, `Hello, ${userName}!`)
+    }
   }
-};
+}
 
-export { greeting };
+export { greeting }
